5b1117f80724f492322615eaa7143a91972972d9
Harman Ark <ark.harman@gmail.com>
2022-03-02 16:56:46 -0500

    Fixed layout and added functionality

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ const adviceNumber = document.getElementById("advice-number")
 const adviceText = document.getElementById("advice-text")
 const diceBtn = document.getElementById("dice-btn")
 
+diceBtn.addEventListener("click", getAdvice)
 
-diceBtn.addEventListener("click", function() {
-    fetch('https://api.adviceslip.com/advice')
-    .then(response => response.json())
-    .then(data => {
-        adviceNumber.innerText = `Advice #${data.slip.id}`
-        adviceText.innerText = data.slip.advice
-    });
-})
+async function getAdvice() {
+    const response = await fetch('https://api.adviceslip.com/advice', {cache: "no-cache"})
+    const data = await response.json()
+    const { id, advice} = data.slip
+    
+    adviceNumber.innerText = `Advice #${id}`
+    adviceText.innerText = advice
+}
+
+window.onload = function() {
+    getAdvice()
+}
